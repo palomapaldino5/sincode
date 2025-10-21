050fed249b7dbd90f4b5acc1b3663cb9c6386d82
palomapaldino5 <pombadapaz@gmail.com>
2025-10-21 11:39:28 -0300

    ok

--- noticias/views.py
+++ noticias/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
 
-#explicar e programar
-# Create your views here.
+# função
+# se def dentro classe = metodo
+#se def fora classe = função
+def index(request):
+    #return HttpResponse("<h1>Alô Django 2025</h1>")
+    return render(request, 'noticiais/index.html')
--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'noticias',
+    'associados',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,8 +114,6 @@ LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 
-USE_I18N = True
-
 USE_TZ = True
 
 
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
