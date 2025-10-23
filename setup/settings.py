6d8ff6c3a26223daa522f87a523976fb5407b7d7
palomapaldino5 <pombadapaz@gmail.com>
2025-10-23 09:07:21 -0300

    Merge remote-tracking branch 'origin/teste' into teste

--- setup/settings.py
+++ setup/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'setup/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
